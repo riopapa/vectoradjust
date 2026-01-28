0c33284e2be8293d53a5a2decd862153313493bc
Woncherl Ha (하원철) <riopapa@gmail.com>
2026-01-28 21:17:49 +0900

    Just to keep source

--- app/src/main/java/com/riopapa/vectoradjust/MainActivity.java
+++ app/src/main/java/com/riopapa/vectoradjust/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tvGo, tvOup;
     String str1, str2, cmd, inpCmd, outCmd, inpPath, outPath;
     float baseX = 9, baseY = 0, val1 = -1, val2 = -1;
-    float scale= 1.2f, xShift = -2f, yShift = -2f;
+    float scale= .9f, xShift = 12f, yShift = 12f;
     String digits = "%.1f";
 
     String xml = "";
